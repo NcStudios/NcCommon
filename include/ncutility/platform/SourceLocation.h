25c29a198a7b5b4b7d48a3f888e72d3d9e150380
Cal Romer <cal.romer@gmail.com>
2023-02-28 17:03:57 -0600

    mac/clang support

--- include/ncutility/platform/SourceLocation.h
+++ include/ncutility/platform/SourceLocation.h
@@ -3,16 +3,25 @@
 #if __has_include(<source_location>)
     #include <source_location>
     #define NC_SOURCE_LOCATION std::source_location
-
 #elif __has_include(<experimental/source_location>)
     #include <experimental/source_location>
     #define NC_SOURCE_LOCATION std::experimental::source_location
 #else
-    #error "<source_location> Missing"
+    struct PlacholderSourceLocation
+    {
+        static constexpr auto msg = "source_location not implemented on this compiler";
+        static constexpr auto current() noexcept { return PlacholderSourceLocation{}; }
+        constexpr auto file_name() const noexcept { return msg; }
+        constexpr auto function_name() const noexcept { return msg; }
+        constexpr auto line() const noexcept { return 0; }
+        constexpr auto column() const noexcept { return 0; }
+    };
+
+    #define NC_SOURCE_LOCATION PlacholderSourceLocation
 #endif
 
-#define NC_SOURCE_LOCATION_CURRENT NC_SOURCE_LOCATION::current()
-#define NC_SOURCE_FILE NC_SOURCE_LOCATION_CURRENT.file_name()
-#define NC_SOURCE_FUNCTION NC_SOURCE_LOCATION_CURRENT.function_name()
-#define NC_SOURCE_LINE NC_SOURCE_LOCATION_CURRENT.line()
-#define NC_SOURCE_COLUMN NC_SOURCE_LOCATION_CURRENT.column()
+    #define NC_SOURCE_LOCATION_CURRENT NC_SOURCE_LOCATION::current()
+    #define NC_SOURCE_FILE NC_SOURCE_LOCATION_CURRENT.file_name()
+    #define NC_SOURCE_FUNCTION NC_SOURCE_LOCATION_CURRENT.function_name()
+    #define NC_SOURCE_LINE NC_SOURCE_LOCATION_CURRENT.line()
+    #define NC_SOURCE_COLUMN NC_SOURCE_LOCATION_CURRENT.column()
